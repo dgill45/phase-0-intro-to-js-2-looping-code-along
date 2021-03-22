bae601221527c66da54a4593bfdf90a57fe8c5eb
donellgill <donell.gill@outlook.com>
2021-03-21 22:16:38 -0400

    wrote functions for index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+const friends = ["Lisa", "Kaitlin", "Jan",];
+    
+function writeCards(friends,event){
+
+    let event = "surprise";
+    let birthday = [];
+    for (let i = 0; i < friends.length; i++){
+        
+        birthday[i] = ("Thank you" + friends + "for the wonderful" + event + "gift!");
+        debugger;
+    }
+    return birthday;
+}
+console.log("Thank you" + ${friends} + "for the wonderful" + ${event} + "gift!");
+writeCards();
+
+console.log(writeCards);
+
+ let num = 10;
+
+function countDown(num){
+   
+    while (num >= 0)
+    console.log(num--);
+    debugger;
+    
+
+}
+
+countDown();
